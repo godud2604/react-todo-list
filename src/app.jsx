bb99454bb4ad3a0bd71f959d1f60b477850c5f0e
godud2604 <ees238@naver.com>
2022-05-22 23:17:05 +0900

    Refactor separation using business logic presentor

--- src/app.jsx
+++ src/app.jsx
@@ -5,54 +5,28 @@ import './app.css';
 import Habits from './components/habits';
 import Navbar from './components/navbar';
 
-const App = () => {
-  const [habits, setHabits] = useState([
-    { id: 1, name: 'Reading', count: 0 },
-    { id: 2, name: 'Running', count: 0 },
-    { id: 3, name: 'Coding', count: 0 },
-  ]);
+const App = ({ presenter }) => {
+  const [habits, setHabits] = useState(presenter.getHabits());
 
   const handleIncrement = useCallback(habit => {
-    setHabits(habits =>
-      habits.map(item => {
-        if (item.id === habit.id) {
-          return { ...habit, count: habit.count + 1 };
-        }
-        return item;
-      })
-    );
-  }, []);
+    presenter.increment(habit, setHabits);
+  }, [presenter]);
 
   const handleDecrement = useCallback(habit => {
-    setHabits(habits =>
-      habits.map(item => {
-        if (item.id === habit.id) {
-          const count = habit.count - 1;
-          return { ...habit, count: count < 0 ? 0 : count };
-        }
-        return item;
-      })
-    );
-  }, []);
+    presenter.decrement(habit, setHabits);
+  }, [presenter]);
 
   const handleDelete = useCallback(habit => {
-    setHabits(habits => habits.filter(item => item.id !== habit.id));
-  }, []);
+    presenter.delete(habit, setHabits);
+  }, [presenter]);
 
   const handleAdd = useCallback(name => {
-    setHabits(habits => [...habits, { id: Date.now(), name, count: 0 }]);
-  }, []);
+    presenter.add(name, setHabits);
+  }, [presenter]);
 
   const handleReset = useCallback(() => {
-    setHabits(habits =>
-      habits.map(habit => {
-        if (habit.count !== 0) {
-          return { ...habit, count: 0 };
-        }
-        return habit;
-      })
-    );
-  }, []);
+    presenter.reset(setHabits);  
+  }, [presenter]);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import HabitPresenter from './habit_presenter.js';
 
 import '@fortawesome/fontawesome-free/js/all.js';
+
+const habitPresenter = new HabitPresenter([
+  { id: 1, name: 'Reading', count: 0 },
+  { id: 2, name: 'Running', count: 0 },
+  { id: 3, name: 'Coding', count: 0 },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App presenter={habitPresenter} />
   </React.StrictMode>
 );
